844cc7e3c54e3e0f0207ec2bddca06a6a98d77ed
Riccardo Inojosa <68616811+Riccino22@users.noreply.github.com>
2022-09-02 11:26:27 -0400

    Create oferta_class.dart
    Rick añadió rentacat/components/oferta_class.dart

---
+++ lib/rentacar/components/oferta_class.dart
@@ -0,0 +1,12 @@
+//Creamos la clase Oferta que se usara cada vez que haya una nueva oferta de auto
+
+class Oferta {
+  String name;
+  int price;
+  int stars;
+  String img;
+  String car;
+  int year;
+  String imageCar;
+  Oferta(this.name, this.price, this.stars, this.img, this.car, this.year, this.imageCar);
+}
