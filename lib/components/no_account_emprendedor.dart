07e4cadd4ed641a0eee710bc33a290927c27e1df
gsuxd <jesusmanuelpolancogarcia51@gmail.com>
2022-12-15 14:18:10 -0400

    fix

--- lib/components/no_account_emprendedor.dart
+++ lib/components/no_account_emprendedor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:joiedriver/register_login_chofer/termin_y_condiciones/terminos_y_condiciones.dart';
 import '../register_login_emprendedor/splash/components/default_button.dart';
 import '../register_login_emprendedor/conts.dart';
 import '../register_login_emprendedor/size_config.dart';
@@ -12,49 +13,46 @@ class NoAccountEmprendedor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(
-        bottom: 20, left: 10, right: 10
-    ),
-    child:  Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          '¿No tienes cuenta Joie Driver?',
-          style: TextStyle(
-            fontSize: getPropertieScreenWidth(18),
-            color: jtextColorSec,
-          ),
-        ),
-        SizedBox(
-          width: getPropertieScreenWidth(10),
-        ),
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        const TerminosCondicionesEmprendedor()));
-          },
-          child: Text(
-            'Registrate',
-            style: TextStyle(fontSize: getPropertieScreenWidth(20)),
-          ),
-        ),
-        SizedBox(
-          width: getPropertieScreenWidth(10),
-        ),
-        ButtonDef(
-            text: "Registrate",
-            press: (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const TerminosCondiciones()));
-            }
-        )
-      ],
-    )
-    );
+        padding: const EdgeInsets.only(bottom: 20, left: 10, right: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '¿No tienes cuenta Joie Driver?',
+              style: TextStyle(
+                fontSize: getPropertieScreenWidth(18),
+                color: jtextColorSec,
+              ),
+            ),
+            SizedBox(
+              width: getPropertieScreenWidth(10),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const TerminosCondicionesEmprendedor()));
+              },
+              child: Text(
+                'Registrate',
+                style: TextStyle(fontSize: getPropertieScreenWidth(20)),
+              ),
+            ),
+            SizedBox(
+              width: getPropertieScreenWidth(10),
+            ),
+            ButtonDef(
+                text: "Registrate",
+                press: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              const TerminosCondicionesChofer()));
+                })
+          ],
+        ));
   }
 }
